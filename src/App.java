28d19e24e841617f67ea862b897d16444028da6f
Juan Portilla <santi2001562001@gmail.com>
2024-10-14 12:45:53 -0500

    first commit

--- src/App.java
+++ src/App.java
@@ -7,16 +7,57 @@ public class App {
 
         MetodosBusqueda mB = new MetodosBusqueda();
 
+        int[] num = new int[100000];
+
         int[] arreglo = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
         mB.printArreglo(arreglo);
 
-        int posicion = mB.busquedaSecuencial(arreglo, 50);
+        //BUSQUEDA SECUENCIA
+        int posSec = mB.busquedaSecuencial(arreglo, 50);
 
-        if(posicion !=-1){
-            System.out.println("El valor se encuentra en la posicion: "+ posicion);
+        if(posSec !=-1){
+            System.out.println("El valor se encuentra en la posicion: "+ posSec);
         }else{
             System.out.println("Valor no encontrado");
         }
+
+
+        //BUSQUEDA BINARIA
+        int posBin = mB.busquedaBinaria(arreglo, 70);
+
+        if(posBin !=-1){
+            System.out.println("El valor se encuentra en la posicion: "+ posBin);
+        }else{
+            System.out.println("Valor no encontrado");
+        }
+
+
+        for(int i = 0; i < num.length; i++ ){
+            num[i] = i+1;
+        }
+
+        long startTime = System.nanoTime();
+        int valSec = 45732;
+        int comparacionesSec = mB.busquedaSecuencial(num, valSec);
+        System.out.println("\nBúsqueda Secuencial:");
+        System.out.println("Valor buscado: " + valSec);
+        System.out.println("Número de comparaciones: " + comparacionesSec);
+        long endTime = System.nanoTime();
+        long totalTime = endTime - startTime;
+
+        System.out.println("Tiempo de ejecucion: " + totalTime + " nanosegundos");
+
+        
+        long startTime1 = System.nanoTime();
+        int valBin = 75224; 
+        int comparacionesBin = mB.busquedaBinaria(num, valBin);
+        System.out.println("\nBúsqueda Binaria:");
+        System.out.println("Valor buscado: " + valBin);
+        System.out.println("Número de comparaciones: " + comparacionesBin);
+        long endTime1 = System.nanoTime();
+        long totalTime1 = endTime1 - startTime1;
+
+        System.out.println("Tiempo de ejecucion: " + totalTime1 + " nanosegundos");
 
     }
 }
--- src/MetodosBusqueda.java
+++ src/MetodosBusqueda.java
@@ -3,12 +3,17 @@ public class MetodosBusqueda {
     public int busquedaSecuencial(int[] arreglo, int valorBuscado){
 
         int pos = -1;
+        int cont = 0;
+
         for(int i = 0; i <arreglo.length; i++){
+            cont++;
             if(arreglo[i] == valorBuscado){
                 pos = i;
+            break;
             }
         }
-        return pos;
+
+        return cont;
 
     }
 
@@ -18,6 +23,31 @@ public class MetodosBusqueda {
             System.out.println(elem+" ");
         }
 
+    }
+
+    public int busquedaBinaria(int[] arreglo, int value){
+
+        int inicio = 0;
+        int fin = arreglo.length -1;
+        int cont=0;
+
+        while (inicio <= fin) {
+            cont++;
+            int medio = inicio + (fin - inicio) /2;
+            
+            if(arreglo[medio] == value){
+                //ELEMENTO ENCONTRADO
+                return cont;
+            }
+
+            if(arreglo[medio] < value){
+                inicio = medio+1;//BUSCAR EN LA MITAD DE LA DERECHA
+            }else{
+                fin = medio-1;//BUSCAR EN LA MITAD DE LA IZQUIERDA
+            }
+        }
+
+        return cont;
     }
 
 }
